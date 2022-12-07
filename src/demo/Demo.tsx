bb30b606dee50e2d2722c265f88b57fa510ef370
Vipin N <vipinsuju@gmail.com>
2022-12-07 20:49:42 +0530

    initial commit

---
+++ src/demo/Demo.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./demo.css";
+import { Button, message, Steps } from "antd";
+
+const steps = [
+  {
+    title: "First",
+    content: "First-content",
+  },
+  {
+    title: "Second",
+    content: "Second-content",
+  },
+  {
+    title: "Last",
+    content: "Last-content",
+  },
+];
+
+const Demo: React.FC = () => {
+  const [current, setCurrent] = useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+  const items = steps.map((item) => ({ key: item.title, title: item.title }));
+
+  return (
+    <>
+      <Steps current={current} items={items} progressDot />
+      <div className="steps-content">{steps[current].content}</div>
+      <div className="steps-action">
+        {current < steps.length - 1 && (
+          <Button type="primary" onClick={() => next()}>
+            Next
+          </Button>
+        )}
+        {current === steps.length - 1 && (
+          <Button
+            type="primary"
+            onClick={() => message.success("Processing complete!")}
+          >
+            Done
+          </Button>
+        )}
+        {current > 0 && (
+          <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
+            Previous
+          </Button>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Demo;
